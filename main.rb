7f608d558935ac994b1f68436790c97d1d671031
Robert Lieck (TheBox) <robert.lieck@gmail.com>
2017-08-16 03:04:30 +0200

    sonic pi files

---
+++ main.rb
@@ -0,0 +1,17 @@
+# This is the main loop. It load/reloads the relevant files and calls the 'song'
+# function in two separate threads in a loop.
+
+in_thread do
+  loop do
+    load "<path_to>/sounds.rb"
+    load "<path_to>/song.rb"
+    sleep 0.25
+  end
+end
+
+in_thread do
+  loop do
+    song
+    sleep 0.1
+  end
+end
---
+++ sounds.rb
@@ -0,0 +1,11 @@
+# Define sounds and other usefull stuff.
+
+def tone(pitch:, duration:, amp:)
+  play pitch, attack: 0.01, decay: duration, sustain: 0.1, release: 0.1, amp: amp
+end
+
+
+def play_beat(sample: :tabla_ghe1, amp: 1)
+  ##| :drum_cymbal_closed :drum_snare_soft :tabla_ghe1
+  sample sample, amp: amp
+end
